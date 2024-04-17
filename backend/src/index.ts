0f65eaecd9bb5db2ce736ae9ccc1269c5d0b68cc
Ahad-23 <ahad.shaikkh@gmail.com>
2024-04-17 22:22:41 +0530

    signin jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,34 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt, sign } from 'hono/jwt'
 const app = new Hono()
 
-app.post('/api/v1/signup',async (c)=>
-  {
-    const prisma = new PrismaClient({
-      //@ts-ignore
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-  })
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+    //@ts-ignore
+		datasourceUrl: c.env.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+    //@ts-ignore
+		const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ 
+      jwt:token 
+    });
+	} 
+  catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
+})
 
 app.post('/api/v1/user/signin', (c) => c.text('signin route'))
 
